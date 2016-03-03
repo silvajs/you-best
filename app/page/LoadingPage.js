193fcc22bd82e5976e767bc60733dfcb51f5c162
Silva <zhoulinspq2006@126.com>
2016-03-03 22:23:47 +0800

    upgrade to 0.21, and animate the loading page

--- app/page/LoadingPage.js
+++ app/page/LoadingPage.js
@@ -5,31 +5,40 @@ import React, {
     TouchableOpacity,
     StyleSheet,
     ToastAndroid,
-    Dimensions
+    Dimensions,
+    Animated,
 } from 'react-native';
 import LoginPage from './LoginPage';
 
 class LoadingPage extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            bounceValue: new Animated.Value(0)
+        };
     }
 
     componentDidMount() {
-        this.timer = setTimeout(function() {
-            this.goLogin();
-        }.bind(this), 1000)
-    }
-
-    componentWillMount() {
-        this.timer && clearTimeout(this.timer);
+        var me = this;
+        this.state.bounceValue.setValue(1.5);
+        Animated.spring(this.state.bounceValue, {
+            toValue: 1,
+            friction: 2,
+            tension: 40
+        }).start(function() {
+            me.goLogin();
+        });
     }
 
     render() {
         var {width, height} = Dimensions.get('window');
+        var imageStyle = {width: width, height: height, transform: [
+            {scale: this.state.bounceValue}
+        ]};
+
         return (
-            <Image source={require('../image/loading_android.png')} style={[styles.container, {width: width, height: height}]}>
-            </Image>
+            <Animated.Image source={require('../image/loading_android.png')}
+                style={[styles.container, imageStyle]}></Animated.Image>
         )
     }
 
